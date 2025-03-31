2cf45da87a4a4c66c151604190bdd422bbf7ce79
Dariazeml1007 <darijazeml@inbox.ru>
2025-04-01 00:10:57 +0300

    Args

--- main.cpp
+++ main.cpp
@@ -5,63 +5,107 @@ const int WIDTH = 800;
 const int HEIGHT = 600;
 const int MAX_ITER = 256;
 const int R_MAX = 4;
+static COLORREF colorBuffer[HEIGHT][WIDTH];
 
-RGBQUAD getColor(int iter);
+RGBQUAD get_color(int iter);
 int mandelbrot(float x0, float y0);
+void run_time(void);
+void draw_mandelbrot (void);
+void count_mandelbrot(void);
+void display(void);
 
-int mandelbrot(float x0, float y0) {
+int main(const int argc, const char* argv[])
+{
+    if (argc > 1 && strcmp(argv[1], "--graphics" ) == 0)
+    {
+        draw_mandelbrot ();
+    }
+    else
+    {
+        run_time();
+    }
+    return 0;
+}
+
+
+int mandelbrot(float x0, float y0)
+{
     float x = 0, y = 0;
     int iter = 0;
-    while (iter < MAX_ITER) {
-        float x2 = x * x;
-        float y2 = y * y;
-
-        if (x2 + y2 >= R_MAX) break;
+    while (iter < MAX_ITER)
+    {
+        float x2 = x * x;  // Re(z_n^2)
+        float y2 = y * y;  // Im(z_n^2)
+        if (x2 + y2 >= R_MAX) break;  // |z_n| ≥ 2
 
         float xy = x * y;
-        x = x2 - y2 + x0;
-        y = 2 * xy + y0;
+        x = x2 - y2 + x0;  // Re(z_(n+1)) = Re(z_n^2) + Re(c)
+        y = 2 * xy + y0;   // Im(z_(n+1)) = Im(z_n^2) + Im(c)
         iter++;
     }
     return iter;
 }
 
-RGBQUAD getColor(int iter)
+RGBQUAD get_color(int iter)
 {
     if (iter == MAX_ITER) return RGBQUAD{0, 0, 0};
 
-    int r = (iter * 55) & 0xFF ;
-    int g = (iter * 256) & 0xFF ;
-    int b = (iter * 125) & 0xFF ;
+    int r = (iter * 197) & 0xFF;
+    int g = (iter * 237) & 0xFF;
+    int b = (iter * 255) & 0xFF;
 
     return RGBQUAD{(BYTE)b, (BYTE)g, (BYTE)r};
 }
 
-int main()
+void draw_mandelbrot (void)
 {
-    txCreateWindow(WIDTH, HEIGHT);
-    txSetFillColor(TX_WHITE);
-    txClear();
+    count_mandelbrot();
+    display();
+}
 
+void count_mandelbrot(void)
+{
     float xMin = -2.5f, xMax = 1.5f;
-    float yMin = -1.5f, yMax = 1.5f;
+    float yMin = -1.5f, yMax = 1.5f ;
 
     for (int py = 0; py < HEIGHT; py++)
     {
         for (int px = 0; px < WIDTH; px++)
         {
-            float x0 = xMin + (xMax - xMin) * px / WIDTH;
-            float y0 = yMin + (yMax - yMin) * py / HEIGHT;
+            float x0 = xMin + (xMax - xMin) * (float)px / (float)WIDTH;
+            float y0 = yMin + (yMax - yMin) * (float)py / (float)HEIGHT;
 
             int iter = mandelbrot(x0, y0);
-            RGBQUAD quad = getColor(iter);
-            COLORREF color = RGB(quad.rgbRed, quad.rgbGreen, quad.rgbBlue);
-            txSetPixel(px, py, color);
+            RGBQUAD quad = get_color(iter);
+            colorBuffer[py][px] = RGB(quad.rgbRed, quad.rgbGreen, quad.rgbBlue);
         }
+    }
+}
 
-        txSleep(1);
+
+void display(void)
+{
+    txCreateWindow(WIDTH, HEIGHT);
+
+    for (int py = 0; py < HEIGHT; py++)
+    {
+        for (int px = 0; px < WIDTH; px++)
+        {
+            txSetPixel(px, py, colorBuffer[py][px]);
+        }
     }
 
     txTextCursor(false);
-    return 0;
+}
+
+
+void run_time(void)
+{
+    clock_t start = clock();
+
+    count_mandelbrot();
+
+    clock_t end = clock();
+    double render_time = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Render time: %.3f seconds\n", render_time);
 }
