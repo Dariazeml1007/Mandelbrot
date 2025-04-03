b02ea5a480b33dbec381fd0bdf598fbdf1fc739e
Dariazeml1007 <darijazeml@inbox.ru>
2025-04-03 17:42:19 +0300

    Add comments

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 #include <cstring>
 #include <ctime>
 #include "txlib.h"
-#include <immintrin.h>  // Для AVX инструкций
+#include <immintrin.h>
 #include <cassert>
 #include <cfenv>
-#define GROUP 8
+#define SIZE_GROUP_AVX 8
 
 const int WIDTH    = 800;
 const int HEIGHT   = 600;
-const int MAX_ITER = 450;
+const int MAX_ITER = 256;
 const float R_MAX  = 4.0f;
 
 typedef struct
 {
-    alignas(32) float x[GROUP];
-    alignas(32) float y[GROUP]; //demand aligned
-    alignas(32) int iter[GROUP];
+    alignas(32) float x[SIZE_GROUP_AVX];
+    alignas(32) float y[SIZE_GROUP_AVX]; //demand aligned
+    alignas(32) int iter[SIZE_GROUP_AVX];
 } PointGroup;
 
 void draw_mandelbrot();
@@ -25,6 +25,7 @@ void display(COLORREF* colorBuffer);
 void run_time();
 void mandelbrot(PointGroup *pixel);
 RGBQUAD get_color(int iter);
+
 
 int main(const int argc, const char* argv[])
 {
@@ -35,17 +36,17 @@ int main(const int argc, const char* argv[])
     return 0;
 }
 
+
 RGBQUAD get_color(int iter)
 {
     if (iter == MAX_ITER) return RGBQUAD{0, 0, 0};
 
-    int r = (iter * 197) & 0xFF;//& 0xFF
+    int r = (iter * 197) & 0xFF;// % 256
     int g = (iter * 237) & 0xFF;
     int b = (iter * 255) & 0xFF;
 
     return RGBQUAD{(BYTE)b, (BYTE)g, (BYTE)r};
 }
-
 
 
 void display(COLORREF* colorBuffer)
@@ -64,6 +65,7 @@ void display(COLORREF* colorBuffer)
     txSleep(10000);
 }
 
+
 void draw_mandelbrot()
 {
     COLORREF* colorBuffer = (COLORREF*)_mm_malloc(HEIGHT * WIDTH * sizeof(COLORREF), 32);
@@ -75,9 +77,10 @@ void draw_mandelbrot()
     _mm_free(colorBuffer);
 }
 
+
 void run_time()
 {
-     COLORREF* colorBuffer = (COLORREF*)_mm_malloc(HEIGHT * WIDTH * sizeof(COLORREF), 32);
+    COLORREF* colorBuffer = (COLORREF*)_mm_malloc(HEIGHT * WIDTH * sizeof(COLORREF), 32);
     if (!colorBuffer) return;
 
     clock_t start = clock();
@@ -88,20 +91,20 @@ void run_time()
     _mm_free(colorBuffer);
 }
 
-void mandelbrot(PointGroup* p)
-{
 
-    assert((uintptr_t)(p->x) % 32 == 0);
-    assert((uintptr_t)(p->y) % 32 == 0);
+void mandelbrot(PointGroup* group)
+{
+    assert((uintptr_t)(group->x) % 32 == 0);
+    assert((uintptr_t)(group->y) % 32 == 0);
 
     __m256 x = _mm256_setzero_ps();
     __m256 y = _mm256_setzero_ps();
-    const __m256 x0 = _mm256_load_ps(p->x);
-    const __m256 y0 = _mm256_load_ps(p->y);
-    const __m256 escape = _mm256_set1_ps(4.0f);
+    const __m256 x0 = _mm256_load_ps(group->x);
+    const __m256 y0 = _mm256_load_ps(group->y);
+    const __m256 escape = _mm256_set1_ps(R_MAX);
 
-    __m256 active = _mm256_set1_ps(-1.0f);  // Все точки активны (0xFFFFFFFF)
-    __m256i res = _mm256_setzero_si256();   // Счётчик итераций
+    __m256 active = _mm256_set1_ps(-1.0f);                       // All points are active (0xFFFFFFFF)
+    __m256i res = _mm256_setzero_si256();                        // matrix of iters
 
     for (int iter = 0; iter < MAX_ITER; iter++)
     {
@@ -111,13 +114,13 @@ void mandelbrot(PointGroup* p)
 
 
         __m256 r2 = _mm256_add_ps(x2, y2);
-        __m256 mask = _mm256_cmp_ps(r2, escape, _CMP_LT_OS);  // 0xFFFFFFFF, если точка ещё в множестве
+        __m256 mask = _mm256_cmp_ps(r2, escape, _CMP_LT_OS);        // 0xFFFFFFFF, if point still active (in Mandelbrot set); 0x00000000 - if not
 
 
-        active = _mm256_and_ps(active, mask);
+        active = _mm256_and_ps(active, mask);                        // byte and to add new "non-active" points to active table
 
 
-        res = _mm256_sub_epi32(res, _mm256_castps_si256(mask)); // sub !!!
+        res = _mm256_sub_epi32(res, _mm256_castps_si256(mask));      // update res
 
 
         __m256 xy = _mm256_mul_ps(x, y);
@@ -125,58 +128,54 @@ void mandelbrot(PointGroup* p)
         y = _mm256_add_ps(_mm256_add_ps(xy, xy), y0);
 
 
-        x = _mm256_mul_ps(x, active); // zeroing not active to not overflow float
+        x = _mm256_mul_ps(x, active);                               // zeroing not active to not overflow float
         y = _mm256_mul_ps(y, active);
 
 
-        if (_mm256_testz_ps(active, active)) break;
+        if (_mm256_testz_ps(active, active)) break;                 // if all points - "inactive"
     }
 
-    _mm256_store_si256((__m256i*)p->iter, res);
+    _mm256_store_si256((__m256i*)group->iter, res);                 // res to group->iter
 }
 
 void count_mandelbrot_grouped(COLORREF* colorBuffer)
 {
-
     const float xMin = -2.5f, xMax = 1.5f;
     const float yMin = -1.5f, yMax = 1.5f;
     const float xScale = (xMax - xMin) / WIDTH;
     const float yScale = (yMax - yMin) / HEIGHT;
 
 
-    PointGroup* pixel = (PointGroup*)_mm_malloc(sizeof(PointGroup), 32);
-    assert(pixel  && "Memory allocation failed");
+    PointGroup* group = (PointGroup*)_mm_malloc(sizeof(PointGroup), 32);
+    assert(group  && "Memory allocation failed");
 
     for (int py = 0; py < HEIGHT; py++) {
         const float y = yMin + (float)py * yScale;
 
-        for (int px = 0; px < WIDTH; px += GROUP)
+        for (int px = 0; px < WIDTH; px += SIZE_GROUP_AVX)
         {
 
-            for (int i = 0; i < GROUP; i++)
+            for (int i = 0; i < SIZE_GROUP_AVX; i++)
             {
                 int curr_px = px + i;
                 if (curr_px >= WIDTH) continue;
-                pixel->x[i] = xMin + (float)curr_px * xScale;
-                pixel->y[i] = y;
+                group->x[i] = xMin + (float)curr_px * xScale;
+                group->y[i] = y;
             }
 
+            mandelbrot(group);
 
-            mandelbrot(pixel);
-
-
-            for (int i = 0; i < GROUP; i++)
+            for (int i = 0; i < SIZE_GROUP_AVX; i++)
             {
-
                 const int curr_px = px + i;
                 if (curr_px >= WIDTH) continue;
 
-                const RGBQUAD quad = get_color(pixel->iter[i]);
+                const RGBQUAD quad = get_color(group->iter[i]);
                 colorBuffer[py * WIDTH + curr_px] = RGB(quad.rgbRed, quad.rgbGreen, quad.rgbBlue);
             }
 
         }
     }
 
-    _mm_free(pixel);
+    _mm_free(group);
 }
